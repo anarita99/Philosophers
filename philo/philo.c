46a410f877cfb3072434c7a16f769f52aa11f1f9
Ana Rita Dores <adores@c1r9s3.42lisboa.com>
2025-09-02 12:15:51 +0100

    idk

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,32 @@
 /*   By: adores <adores@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 14:11:49 by adores            #+#    #+#             */
-/*   Updated: 2025/08/13 14:11:50 by adores           ###   ########.fr       */
+/*   Updated: 2025/08/26 12:58:07 by adores           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <pthread.h>
+
+void *computation(void *add)
+{
+	long *addnum = (long *)add;
+	printf("Add: %ld\n", *addnum);
+	return (NULL);
+}
+
+int main()
+{
+	pthread_t thread1;
+	pthread_t thread2;
+	long value1 = 1;
+	long value2 = 2;
+	pthread_create(&thread1, NULL, computation, (void*) &value1);
+	pthread_create(&thread2, NULL, computation, (void*) &value2);
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+	return(0);
+}
+
+
+/*gcc -g -pthread main.c*/
