e76264ed099c72865253eed334021bd633a9a962
Ana Rita Dores <adores@c1r10s1.42lisboa.com>
2025-09-02 16:43:43 +0100

    idk

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: adores <adores@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 14:12:07 by adores            #+#    #+#             */
-/*   Updated: 2025/09/02 12:52:42 by adores           ###   ########.fr       */
+/*   Updated: 2025/09/02 16:01:09 by adores           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 #include <sys/time.h> //gettimeofday
 #include <limits.h> // INTMAX
 #include <pthread.h>
+#include <stdbool.h>
 
+typedef struct s_data t_data;
 typedef struct s_fork
 {
 	pthread_mutex_t	fork;
@@ -28,15 +30,29 @@ typedef struct s_philo
 	int		philo_id;
 	t_fork	*left_fork;
 	t_fork	*right_fork;
-	long	meals;
+	long	meals_count;
 	bool	full;
 	long	last_meal_time;
 	pthread_t	thread_id; //philo is a thread
+	t_data	*data;
 }	t_philo;
+
+typedef struct s_data
+{
+	long	n_philo;
+	long	time_to_die;
+	long	time_to_eat;
+	long	time_to_sleep;
+	long	limit_meals;
+	long	start_simulation;
+	bool	end_simulation; // a philo dies or all philos full
+	t_fork	*forks;
+	t_philo	*philos;
+}t_data;
 
 
 
 /*
 FORK = MUTEX
-./philo 8 800 200 200
+./philo 8 800 200 200 [5]
 */
